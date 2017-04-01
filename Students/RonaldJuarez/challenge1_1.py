63734721ee9f2202bc362700065bb1d3646cc3b1
Ronald Juarez <ronald.juarez.chambi@gmail.com>
2017-04-01 00:55:50 -0500

    Submitting Ronald Juarez Folder

--- Students/RonaldJuarez/challenge1_1.py
+++ Students/RonaldJuarez/challenge1_1.py
@@ -1,10 +1,19 @@
-import random;
+import random
+
+HEAD = 1
+TAIL = 0 
+
+############################# PART 1 ################################
+
+prob = 0.7;
+iterations = 1000;
 
 def biasedcoinflip(p = 0.5):
 	
 	# Create method for biased coin flip
     # Return 1 for heads, with probability p
     # and 0 for tails
+	# Author: Ronald Juarez. ECEN303. Spring2017
 	
 	result = 0;
 	flip = random.uniform(0,1);
@@ -14,6 +23,20 @@ def biasedcoinflip(p = 0.5):
 	else:
 		result = 0;
 	
-	print result;
-	
-biasedcoinflip();
+	return result;
+
+
+heads= 0;
+
+for i in range(iterations):
+	result = biasedcoinflip(prob)
+	if result==HEAD:
+		heads+=1
+		
+#Calculate empirical probability
+print "Pr(heads): " + str(heads/float(iterations))
+print "Pr(tails): " + str((iterations-heads)/float(iterations))
+
+
+
+############################# PART 2 ################################
